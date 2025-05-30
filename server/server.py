8b3826ccc089bb138780f50786da8d6ac37805c7
kubamaz <64901069+kubamaz@users.noreply.github.com>
2025-05-30 12:28:55 +0200

    Dodanie timeouta aby umożlwić wyłączenie serwera

--- server/server.py
+++ server/server.py
@@ -8,11 +8,15 @@ def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
     server.listen()
+    server.settimeout(1.0) 
+
     print(f"[SERVER] Nasłuchuję na {HOST}:{PORT}")
 
     while True:
         try: 
             connection, _ = server.accept()
+        except socket.timeout:
+            pass
         except Exception as e:
             print("Błąd połączenie klienta: ", e)
 
