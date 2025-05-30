7a84edd3eb9b215dad9ead3e64b4eb4b10384ac2
kubamaz <64901069+kubamaz@users.noreply.github.com>
2025-05-30 12:22:14 +0200

    Stworzenie serwera TCP

---
+++ server/server.py
@@ -0,0 +1,19 @@
+import socket
+
+
+HOST = '0.0.0.0'
+PORT = 12345
+
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen()
+    print(f"[SERVER] Nasłuchuję na {HOST}:{PORT}")
+
+    while True:
+        try: 
+            connection, _ = server.accept()
+        except Exception as e:
+            print("Błąd połączenie klienta: ", e)
+
+start_server()
