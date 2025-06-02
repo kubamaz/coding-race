a8bd0a30ffb30676857da4c7f2396e3884557471
kubamaz <64901069+kubamaz@users.noreply.github.com>
2025-06-02 21:09:52 +0200

    Dodano sprawdzenie czy klient jest połączony z serwerem

--- server/client_test.py
+++ server/client_test.py
@@ -1,13 +1,24 @@
 import socket
-
+import time
 SERVER_IP = '127.0.0.1'
 PORT = 12345
+
+isConnected = False
 
 try:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((SERVER_IP, PORT))
+    isConnected = True
 except Exception as e:
     print(f"Error connecting to server: {e}")
 
-while True:
+while isConnected:
+    try:
+        client.sendall(b"TEST")
+    except Exception as e:
+        print(f"Error sending data: {e}")
+        isConnected = False
+        break
+    print("Połączono.")
+    time.sleep(1)
     pass
