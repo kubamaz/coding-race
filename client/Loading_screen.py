f901804995e3e3c3ec5baac1bd454af4c3bfe492
Karolina Banaszewska <kbanaszewskaa@gmail.com>
2025-06-06 14:12:32 +0200

    Dodanie czterech samochodzikow jadacych jednoczesnie

--- client/Loading_screen.py
+++ client/Loading_screen.py
@@ -18,11 +18,12 @@ yellow_car = Car(resize_img("assets/imgs/yellow-car-side.png", CAR_HEIGHT, CAR_W
 green_car = Car(resize_img("assets/imgs/green-car-side.png", CAR_HEIGHT, CAR_WIDTH), BEGIN_X, BEGIN_Y)
 blue_car = Car(resize_img("assets/imgs/blue-car-side.png", CAR_HEIGHT, CAR_WIDTH), BEGIN_X, BEGIN_Y)
 
-cars = [red_car, yellow_car, green_car, blue_car, red_car, yellow_car, green_car, blue_car]
+cars = [red_car, yellow_car, green_car, blue_car]
 
 car_number1 = 0
 car_number2 = -1
-round = 1
+car_number3 = -1
+car_number4 = -1
 
 loading = True
 
@@ -38,22 +39,34 @@ while loading:
     if car_number2 >= 0 : 
         screen.blit(cars[car_number2].img, (cars[car_number2].x, cars[car_number2].y))
         cars[car_number2].x += 2  
+    if car_number3 >= 0 : 
+        screen.blit(cars[car_number3].img, (cars[car_number3].x, cars[car_number3].y))
+        cars[car_number3].x += 2  
+    if car_number4 >= 0 : 
+        screen.blit(cars[car_number4].img, (cars[car_number4].x, cars[car_number4].y))
+        cars[car_number4].x += 2  
 
-    if round == 1 and cars[car_number1].x > SCREEN_WIDTH//2: #Dokladam nowy samochodzik w polowie
+    if car_number2 == -1 and cars[car_number1].x > SCREEN_WIDTH//4: #Dokladam nowy samochodzik w polowie
         car_number2+=2
-        round = 2
+
+    if car_number3 == -1 and cars[car_number1].x > SCREEN_WIDTH//2: #Dokladam nowy samochodzik w polowie
+        car_number3+=3
+
+    if car_number4 == -1 and cars[car_number1].x > (SCREEN_WIDTH//2 + SCREEN_WIDTH//4): #Dokladam nowy samochodzik w polowie
+        car_number4+=4
 
     if cars[car_number1].x > SCREEN_WIDTH:
-        cars[car_number1].x = 0
-        car_number1+=2
-        if car_number1 == NUMBER_OF_CARS:
-            car_number1 = 0
+        cars[car_number1].x = BEGIN_X
 
-    if cars[car_number2].x > SCREEN_WIDTH:
-        cars[car_number2].x = 0
-        car_number2+=2
-        if car_number2 == NUMBER_OF_CARS:
-            car_number2 = 0
+    if car_number2 != -1 and cars[car_number2].x > SCREEN_WIDTH:
+        cars[car_number2].x = BEGIN_X
+
+    
+    if car_number3!= -1 and cars[car_number3].x > SCREEN_WIDTH:
+        cars[car_number3].x = BEGIN_X
+
+    if car_number4!= -1 and cars[car_number4].x > SCREEN_WIDTH:
+        cars[car_number4].x = BEGIN_X
 
     pygame.display.update()
     clock.tick(60)
